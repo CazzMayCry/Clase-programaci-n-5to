dae289e2ea4480f9da77cc25599b4d4c5f0c73b1
Carolina Mercado <karolina020503@gmail.com>
2023-08-28 22:41:38 -0500

    Más correccion de código

--- Programacion repaso/Assets/Scripts/Prueba.cs
+++ Programacion repaso/Assets/Scripts/Prueba.cs
@@ -19,27 +19,22 @@ public class Animal : MonoBehaviour
 
 public class Perro : Animal
 {
-    // Método que se ejecuta al inicio (parte de la herencia)
     private void Start()
     {
         Nombre = "Perro";
+        EmitirSonido("¡Guau guau!");  // Llamar al método directamente
     }
 
-    // Polimorfismo: Sobrescribir el método EmitirSonido
     public override void EmitirSonido(string sonido)
     {
+        base.EmitirSonido(sonido);  // Llamar al método de la clase base
         Debug.Log($"{Nombre} ladra: {sonido}");
     }
 }
-
 public class Ejemplo : MonoBehaviour
 {
     private void Start()
     {
-        // Crear una instancia de Perro (uso del constructor)
         Perro miPerro = new Perro();
-
-        // Llamar al método EmitirSonido utilizando polimorfismo
-        miPerro.EmitirSonido("¡Guau guau!");  // Uso del método y el parámetro
     }
 }
